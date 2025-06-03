aa7fcebcb323786cf96905bf79f5d3a2db2fc9de
ReloJunior <andresrodriguezab@hotmail.com>
2025-06-03 18:22:08 -0300

    footer section

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+    <div className="w-11/12 pt-48 md:pt-0 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
       <div>
         <Image src={assets.profile_img} alt="" className="rounded-full w-48" />
       </div>
