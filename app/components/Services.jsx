c6f96983bbb0ee497749f6d63f9d419ed0d7ea17
ReloJunior <andresrodriguezab@hotmail.com>
2025-06-04 20:57:49 -0300

    animations of Services section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -2,22 +2,55 @@ import { serviceData } from "@/assets/assets";
 import React from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
+import { motion } from "motion/react";
 
 const Services = () => {
   return (
-    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-ovo">What I offer</h4>
-      <h2 className="text-center text-5xl font-ovo"> My Services </h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.6 }}
+        className="text-center mb-2 text-lg font-ovo"
+      >
+        What I offer
+      </motion.h4>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}
+        className="text-center text-5xl font-ovo"
+      >
+        My Services
+      </motion.h2>
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo"
+      >
         I offer a range of web development services to help you build and enhance your online presence. Whether you need
         a new website, a redesign, or ongoing maintenance, I can provide tailored solutions to meet your needs. My
         services include:
-      </p>
+      </motion.p>
 
-      <div className="grid autoGrid gap-6 my-10">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.9 }}
+        className="grid autoGrid gap-6 my-10"
+      >
         {serviceData.map(({ icon, title, description, link }, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
             key={index}
             className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black shadow-2xl cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 dark:hover:bg-[#2a004a] dark:hover:shadow-white"
           >
@@ -27,10 +60,10 @@ const Services = () => {
             <a href={link} className="">
               Read more <Image src={assets.right_arrow} alt="" className="w-4" />
             </a>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
