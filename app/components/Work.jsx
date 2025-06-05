741faf1ee18e22101f35785d45503efd82b0fccb
ReloJunior <andresrodriguezab@hotmail.com>
2025-06-04 21:05:32 -0300

    work section motions

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -3,31 +3,66 @@ import React from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 import { useTheme } from "next-themes";
+import { motion } from "motion/react";
 
 const Work = () => {
   const resolvedTheme = useTheme();
 
   const arrowIcon = resolvedTheme === "dark" ? assets.right_arrow_bold_dark : assets.right_arrow_bold;
-  
-  return (
-    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-ovo">My portfolio</h4>
-      <h2 className="text-center text-5xl font-ovo"> My latests projects</h2>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="work"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-ovo"
+      >
+        My portfolio
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center text-5xl font-ovo"
+      >
+        My latests projects
+      </motion.h2>
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.8 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo"
+      >
         Here are some of my latest projects that showcase my skills in web development. Each project demonstrates my
         ability to create responsive, user-friendly interfaces and implement modern web technologies. Feel free to
         explore them and see the quality of my work.
-      </p>
+      </motion.p>
 
-      <div className="grid autoGrid gap-5 my-10 dark:text-black">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.7, delay: 0.9 }}
+        className="grid autoGrid gap-5 my-10 dark:text-black"
+      >
         {workData.map((project, index) => (
           <div
             key={index}
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
             style={{ backgroundImage: `url(${project.bgImage})` }}
           >
-            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-300 group-hover:bottom-7">
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.3 }}
+              className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-300 group-hover:bottom-7"
+            >
               <div>
                 <h2 className="font-semibold">{project.title}</h2>
                 <p className="text-sm text-gray-700">{project.description}</p>
@@ -35,15 +70,21 @@ const Work = () => {
               <div className="border rounded-full border-black w-9 asoect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
                 <Image src={assets.send_icon} alt="Send Icon" className="w-5" />
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
-      </div>
+      </motion.div>
 
-      <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:lightHover duration-500 dark:text-white dark:border-white dark:hover:darkHover">
+      <motion.a
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1.2 }}
+        href=""
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:lightHover duration-500 dark:text-white dark:border-white dark:hover:darkHover"
+      >
         Show more <Image src={arrowIcon} alt="Right Arrow" className="w-4" />
-      </a>
-    </div>
+      </motion.a>
+    </motion.div>
   );
 };
 
